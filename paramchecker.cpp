004afaed293d9bddb0cf8d46c40857bb2f0d8e0f
sruthirajkoodathil <68430130+sruthirajkoodathil@users.noreply.github.com>
2020-07-20 15:32:01 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,29 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
+bool HeartRateOk(float bpm) {
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
+  } 
+  return true;
+}
+bool Spo2Ok(float spo2) {
+  if(spo2 < 80) {
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  } 
+  return true;
+}
+
+bool RespRateOk(float respRate) {
+  if(respRate < 30 || respRate > 60) {
     return false;
   }
   return true;
 }
+
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  if(HeartRateOk(bpm) & Spo2Ok(spo2) & RespRateOk(respRate){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
