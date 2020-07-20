c8051bcbe0aca4e7b6b81dcdbc06158a28a3430d
sruthirajkoodathil <68430130+sruthirajkoodathil@users.noreply.github.com>
2020-07-20 20:52:35 +0530

    Update paramchecker.h

--- paramchecker.h
+++ paramchecker.h
@@ -1,4 +1,5 @@
-bool HeartRateOk(float bpm);
-bool Spo2Ok(float spo2);
-bool RespRateOk(float respRate);
+bool bpmOK, spo2OK, resOK;
+void HeartRateOk(float bpm);
+void Spo2Ok(float spo2);
+void RespRateOk(float respRate);
 bool vitalsAreOk(float bpm, float spo2, float respRate);
