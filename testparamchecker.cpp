8d62be854ad3cbf13bd9aaa92727137aceb6f088
sruthirajkoodathil <68430130+sruthirajkoodathil@users.noreply.github.com>
2020-07-22 21:10:54 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,17 +1,17 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
  
-struct param Vitals_3_valid[]={
+struct param Vitals_3_valid[3]={
 {100, 50, 100},
 {80,40,100},
 {80,40,100},
-}
-struct param Vitals_4_Novalid[]={
+};
+struct param Vitals_4_Novalid[4]={
 {40, 50, 100},
 {150,40,100},
 {20,40,100},
 {101,20,100},
-}
+};
 TEST(VitalsTest, Vitals_3) { 
     ASSERT_EQ(true, vitalsAreOk(Vitals_3_valid,3));
 }
