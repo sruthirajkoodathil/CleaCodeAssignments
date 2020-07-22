6469a8e72dab6eeaf2f93f2c3d3e96075219f4b7
sruthirajkoodathil <68430130+sruthirajkoodathil@users.noreply.github.com>
2020-07-22 19:50:19 +0530

    test updated

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,12 +1,23 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
  
-TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
+param Vitals_3_valid[]{
+{100, 50, 100},
+{80,40,100},
+{80,40,100},
+}
+param Vitals_4_Novalid[]{
+{40, 50, 100},
+{150,40,100},
+{20,40,100},
+{101,20,100},
+}
+TEST(VitalsTest, Vitals_3) { 
+    ASSERT_EQ(true, vitalsAreOk(Vitals_3_valid,3);
 }
  
-TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+TEST(VitalsTest, Vitals_4) {
+    ASSERT_EQ(false, vitalsAreOk(Vitals_4_Novalid, 4));
 }
  
 int main(int argc, char **argv) {
